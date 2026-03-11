e9c1cf1b3c77fae8fb802bcf5ec5ee371552e808
kim minseong <msjw9022@naver.com>
2026-03-11 23:25:39 +0900

    [COW-1] CHORE - Husky 및 commitlint 설정

---
+++ commitlint.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  parserPreset: { // parserOpts를 parserPreset 안에 감싸는 것이 정석 이라고 합니다.!!
+    parserOpts: {
+      headerPattern: /^\[([^\]]+)\]\s+(FEAT|FIX|DOCS|STYLE|REFACTOR|TEST|CHORE)\s+-\s+(.*)$/,
+      headerCorrespondence: ['card', 'type', 'subject'],
+    },
+  },
+  rules: {
+    'header-max-length': [2, 'always', 100],
+    'type-enum': [
+      2,
+      'always',
+      ['FEAT', 'FIX', 'DOCS', 'STYLE', 'REFACTOR', 'TEST', 'CHORE'],
+    ],
+    'type-case': [2, 'always', 'upper-case'], // 대문자 허용을 위해 
+    'subject-empty': [2, 'never'],            // 제목이 비어있으면 안 됨
+    'type-empty': [2, 'never'],               // 타입이 비어있으면 안 됨
+  },
+};
