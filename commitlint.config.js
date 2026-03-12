9e0f0a258ba844c1fa3a7bc3b56a496b234227be
minha <luck7118@naver.com>
2026-03-12 10:56:07 +0900

    [COW-1] CHORE - FSD 폴더 구조 초기화 및 .gitkeep 추가

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  parserPreset: { // parserOpts를 parserPreset 안에 감싸는 것이 정석 이라고 합니다.!!
+  parserPreset: {
     parserOpts: {
       headerPattern: /^\[([^\]]+)\]\s+(FEAT|FIX|DOCS|STYLE|REFACTOR|TEST|CHORE)\s+-\s+(.*)$/,
       headerCorrespondence: ['card', 'type', 'subject'],
@@ -8,13 +8,9 @@ module.exports = {
   },
   rules: {
     'header-max-length': [2, 'always', 100],
-    'type-enum': [
-      2,
-      'always',
-      ['FEAT', 'FIX', 'DOCS', 'STYLE', 'REFACTOR', 'TEST', 'CHORE'],
-    ],
-    'type-case': [2, 'always', 'upper-case'], // 대문자 허용을 위해 
-    'subject-empty': [2, 'never'],            // 제목이 비어있으면 안 됨
-    'type-empty': [2, 'never'],               // 타입이 비어있으면 안 됨
+    'type-enum': [2, 'always', ['FEAT', 'FIX', 'DOCS', 'STYLE', 'REFACTOR', 'TEST', 'CHORE']],
+    'type-case': [2, 'always', 'upper-case'], // 대문자 허용을 위해
+    'subject-empty': [2, 'never'], // 제목이 비어있으면 안 됨
+    'type-empty': [2, 'never'], // 타입이 비어있으면 안 됨
   },
 };
