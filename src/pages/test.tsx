99342b5d390c15c5a9b37bd248bb0435b9142f28
YeomSeunghyeon <100lion67@gmail.com>
2026-03-14 19:46:25 +0900

    [COW-11]feat:imagepicker 컴포넌트 제작

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,1 +1,15 @@
-// 앞으로 사용할 임시 테스트 페이지 ㅇ
+// 앞으로 사용할 임시 테스트 페이지 
+
+import { ImagePicker } from "@/shared/ui/imagePicker/ImagePicker";
+import { useState } from "react";
+
+export default function test(){
+const [images, setImages] = useState<File[]>([])
+
+return(
+  <div className="flex mt-12 justify-center">
+<ImagePicker max={5} onChange={setImages} />
+
+</div>
+)
+}
