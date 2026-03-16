bc20b3660f447b0f86c381aee6c34f72f38201cb
YeomSeunghyeon <100lion67@gmail.com>
2026-03-16 18:27:00 +0900

    [COW-17] skeleton 공통 컴포넌트

--- src/pages/test/yeom.tsx
+++ src/pages/test/yeom.tsx
@@ -1,3 +1,11 @@
-export default function TestPage() {
-  return <div></div>;
+import { Skeleton } from "@/shared/ui/skeleton/Skeleton"
+
+export default function PostCardSkeleton() {
+  return (
+    <div className="space-y-3 rounded-lg border p-4">
+      <Skeleton className="h-6 w-2/3" />
+
+      <Skeleton className="h-40 w-full rounded-md" />
+    </div>
+  )
 }
