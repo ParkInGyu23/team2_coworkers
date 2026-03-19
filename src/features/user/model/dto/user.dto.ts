e87716ff99434e243e978ad57f3c0331694b5147
minha <luck7118@naver.com>
2026-03-19 22:59:51 +0900

    [COW-37] REFACTOR - 미사용 임포트 제거

--- src/features/user/model/dto/user.dto.ts
+++ src/features/user/model/dto/user.dto.ts
@@ -1,6 +1,5 @@
 import type { RecurrenceType } from '@/features/task/model/types/recurrence.type';
 import type { Role } from '../types/role.type';
-import type { User } from '@/shared/types/user.model';
 
 export interface UserDto {
   id: number;
