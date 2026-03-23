45b332a961bb0fef82832f5f3a14b0892be2b7ec
minha <luck7118@naver.com>
2026-03-23 14:23:53 +0900

    [COW-57] FEAT - 오타 수정 및 그룹 생성 로직 피드백 반영

--- src/features/group/api/getGroup.ts
+++ src/features/group/api/getGroup.ts
@@ -3,11 +3,11 @@ import type { GetGroupDetailResponse } from '../model/dto/group.dto';
 import type { GroupDetail } from '../model/entities/group.model';
 import { toGroupDetail } from '../lib/mappers/group.mapper';
 
-export interface GetgroupParams {
+export interface GetGroupParams {
   groupId: number;
 }
 
-export async function getGroup({ groupId }: GetgroupParams): Promise<GroupDetail> {
+export async function getGroup({ groupId }: GetGroupParams): Promise<GroupDetail> {
   const { data } = await clientFetcher.get<GetGroupDetailResponse>(`/groups/${groupId}`);
   return toGroupDetail(data);
 }
--- src/features/group/hook/useCreateGroupMutation.ts
+++ src/features/group/hook/useCreateGroupMutation.ts
@@ -4,6 +4,7 @@ import { createGroup } from '../api/createGroup';
 import { ApiError } from '@/shared/types/apiError';
 import { Group } from '../model/entities/group.model';
 import { USER_QUERY_KEYS } from '@/features/user/lib/queryKeys';
+import { GROUP_QUERY_KEYS } from '../lib/queryKeys';
 
 export function useCreateGroupMutation() {
   const queryClient = useQueryClient();
@@ -13,6 +14,7 @@ export function useCreateGroupMutation() {
     onSuccess: (newGroup) => {
       // 전체 그룹 목록 새로고침 사이드바 업데이트용
       queryClient.invalidateQueries({ queryKey: USER_QUERY_KEYS.groups() });
+      queryClient.invalidateQueries({ queryKey: GROUP_QUERY_KEYS.all });
     },
   });
 }
