9f59ab72d01c4d8953ea34af33c2cf151317eeca
minha <luck7118@naver.com>
2026-03-26 15:28:21 +0900

    [COW-77] REFACTOR - 회원 탈퇴 보안 로직 개선 훅(useSignOut) 재사용

--- src/features/user/hooks/useDeleteUserMutation.ts
+++ src/features/user/hooks/useDeleteUserMutation.ts
@@ -1,23 +1,21 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { deleteUser } from '../api/deleteUser';
 import { ApiError } from '@/shared/types/apiError';
-import { authKeys } from '@/shared/lib/queryKeys/authKeys';
 import { useRouter } from 'next/router';
+import { useSignOut } from '@/features/auth/hooks/useSignOut';
 
 export function useDeleteUserMutation() {
-  const queryClient = useQueryClient();
   const router = useRouter();
+  const { mutate: signOut } = useSignOut();
 
   return useMutation<void, ApiError, void>({
     mutationFn: deleteUser,
     onSuccess: () => {
-      queryClient.clear();
-      queryClient.setQueryData(authKeys.me(), null);
-
-      document.cookie = 'accessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-      document.cookie = 'refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-
-      router.replace('/');
+      signOut(undefined, {
+        onSuccess: () => {
+          router.replace('/');
+        },
+      });
     },
   });
 }
