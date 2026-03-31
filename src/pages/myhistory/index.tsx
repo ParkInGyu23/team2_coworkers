3925ed171d221566d8e472ed236a228fbadd7acc
minha <luck7118@naver.com>
2026-03-31 15:33:35 +0900

    [COW-98] FEAT - 마이 히스토리 페이지 UI 및 월별/카테고리별 필터링 구현

---
+++ src/pages/myhistory/index.tsx
@@ -0,0 +1,27 @@
+import { GlobalLayout } from '@/widgets/layout/GlobalLayout';
+import { HistoryBoard } from '@/features/history/ui/HistoryBoard';
+import { cn } from '@/shared/lib/cn';
+
+export default function MyHistoryPage() {
+  return (
+    <GlobalLayout>
+      {/* TODO: 아직 머지 전이라 추후 팀 헤더 추가 예정*/}
+      {/* <Header /> */}
+      <div className="bg-background-secondary min-h-screen w-full">
+        <main
+          className={cn(
+            'flex w-full flex-col',
+            'pt-5 md:pt-16 xl:pt-[90px]',
+            'px-[16px] md:px-[26px] xl:px-0 xl:pl-[85px]',
+          )}
+        >
+          <div className="mb-6 w-full md:mb-5.5 xl:mb-12">{/* <TeamHeader /> */}</div>
+
+          <div className="flex w-full justify-center xl:justify-start">
+            <HistoryBoard />
+          </div>
+        </main>
+      </div>
+    </GlobalLayout>
+  );
+}
